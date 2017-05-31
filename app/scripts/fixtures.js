f38b9c2c7a92122f31a55d442aa0e177d25f2d11
PRIYAM PATEL <PATELPRIYAM203@GMAIL.COM>
2017-05-31 13:18:08 -0400

    Checkpoint 5 Angular Controllers: Completed

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
+        { name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
+        { name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
+        { name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink'},
+        { name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { name: 'Hello, Operator?', length: '1:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
+        { name: 'Fits in your pocket', length: '3:21'},
+        { name: 'Can you hear me now?', length: '3:14' },
+        { name: 'Wrong phone number', length: '2:15'}
+    ]
+};
+
+
+var albumAnjunaBeats = {
+  title: 'AnjunaBeats WorldWide 06',
+  artist: 'Jaosn Ross',
+  label: 'AnjunaBeats',
+  year: '2017',
+  albumArtUrl: 'assets/images/album_covers/21.png',
+  songs: [
+      { name: 'Begin Again', length: '1:31' },
+      { name: 'You\'ll know', length: '5:20' },
+      { name: 'Legacy', length: '5:05'},
+      { name: 'SOS', length: '5:34' },
+      { name: 'Universal', length: '4:47'}
+  ]
+};

8581a144d9317480a9fac5052c6e87339bd520e8
PRIYAM PATEL <PATELPRIYAM203@GMAIL.COM>
2017-06-05 21:16:11 -0400

    Checkpoint 10 Angular Directives Part 2: Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
           var SongPlayer = {};
 
           /**
@@ -30,7 +30,20 @@
                   preload: true
               });
 
+              currentBuzzObject.bind('timeupdate', function() {
+                  $rootScope.$apply(function() {
+                      SongPlayer.currentTime = currentBuzzObject.getTime();
+                  });
+              });
+
               SongPlayer.currentSong = song;
+
+              /**
+              * @desc Current playback time (in seconds) of currently playing song
+              * @type {Number}
+              */
+              SongPlayer.currentTime = null;
+
           };
 
           /**
@@ -123,12 +136,23 @@
                   setSong(song);
                   playSong(song);
               }
-          }
+          };
+
+          /**
+          * @function setCurrentTime
+          * @desc Set current time (in seconds) of currently playing song
+          * @param {Number} time
+          */
+          SongPlayer.setCurrentTime = function(time) {
+              if (currentBuzzObject) {
+                  currentBuzzObject.setTime(time);
+              }
+          };
 
           return SongPlayer;
      }
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
